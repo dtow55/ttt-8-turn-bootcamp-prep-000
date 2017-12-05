cec9f4389aec6ecdcc70af9685a0f58612fa9bfd
David J Towster <djt4xe@virginia.edu>
2017-12-05 19:58:33 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,29 @@
+def turn
+#ask for input
+
+#get input
+
+#convert input to index
+
+end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+  input.to_i - 1
+end
+
+def move(board, index, character = "X")
+  board[index] = character
+  board
+end
+
+def valid_move?(index)
+  
+end
